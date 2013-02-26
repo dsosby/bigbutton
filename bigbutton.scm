bdb963a26e693d7df3e23b8dd65f498457427635
David Sosby <david@sosby.org>
2013-02-25 23:24:40 -0600

    Trying to use the template

--- bigbutton.scm
+++ bigbutton.scm
@@ -1,11 +1,18 @@
-(use awful
-     ersatz-lib)
+(use awful)
+(include "ezersatz.scm")
+(import ezersatz)
 
-(enable-sxml #t)
+(enable-sxml #f)
+
+;; Fill the template with the given data
+(define (standard-page data #!optional (file "index.html") (page-title "BigButton"))
+  (ezersatz file
+	    (if (string? data) `((content . ,data)) data)))
 
 (define (get-greeting-string salutation #!optional [name "World"])
   (string-append salutation ", " name))
 
 (define-page (main-page-path)
   (lambda ()
-    (get-greeting-string "Hello" ($ 'person "world"))))
+    (standard-page 
+	(get-greeting-string "Hello" ($ 'person "world")))))
