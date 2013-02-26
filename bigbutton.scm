fbb01b58e5027eccbcac4d576a451e34ec18b65e
David Sosby <david@sosby.org>
2013-02-25 23:33:32 -0600

    Use a standard page generating function

--- bigbutton.scm
+++ bigbutton.scm
@@ -7,12 +7,13 @@
 ;; Fill the template with the given data
 (define (standard-page data #!optional (file "index.html") (page-title "BigButton"))
   (ezersatz file
-	    (if (string? data) `((content . ,data)) data)))
+	    (if (string? data) `((content . ,data) (page_title . ,page-title))
+		               (alist-cons 'page_title page-title data))))
 
 (define (get-greeting-string salutation #!optional [name "World"])
   (string-append salutation ", " name))
 
 (define-page (main-page-path)
   (lambda ()
-    (standard-page 
+      (standard-page
 	(get-greeting-string "Hello" ($ 'person "world")))))
