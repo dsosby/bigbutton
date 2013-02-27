7a03691282196a10f6378461a83306b40fc87568
David Sosby <david@sosby.org>
2013-02-26 20:26:06 -0600

    Add debug logging and fix compilation bug

--- bigbutton.scm
+++ bigbutton.scm
@@ -3,16 +3,21 @@
 (import ezersatz)
 
 (enable-sxml #f)
+(when (development-mode?)
+  (debug-file "debug.log"))
 
 ;; Fill the template with the given data
 (define (standard-page data #!optional (file "index.tmpl") (page-title "BigButton"))
-  (lambda () 
     (ezersatz file
 	      (if (string? data) `((content . ,data) (page_title . ,page-title))
-				 (alist-cons 'page_title page-title data)))))
+				 (alist-cons 'page_title page-title data))))
 
 (define (get-greeting-string salutation #!optional [name "World"])
   (string-append salutation ", " name))
 
 (define-page (main-page-path)
-  (standard-page (get-greeting-string "Hello" "world")))
+  (lambda () 
+    (let* ([whom ($ 'whom "world")]
+	   [greeting (get-greeting-string "Hello" whom)])
+      (debug whom)
+      (standard-page greeting))))
