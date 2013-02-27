23f82b1bd0367e7c238ae8bb22f41ba7e9c422b2
David Sosby <david@sosby.org>
2013-02-26 20:18:12 -0600

    Bugs

--- bigbutton.scm
+++ bigbutton.scm
@@ -6,12 +6,13 @@
 
 ;; Fill the template with the given data
 (define (standard-page data #!optional (file "index.tmpl") (page-title "BigButton"))
-  (ezersatz file
-	    (if (string? data) `((content . ,data) (page_title . ,page-title))
-		               (alist-cons 'page_title page-title data))))
+  (lambda () 
+    (ezersatz file
+	      (if (string? data) `((content . ,data) (page_title . ,page-title))
+				 (alist-cons 'page_title page-title data)))))
 
 (define (get-greeting-string salutation #!optional [name "World"])
   (string-append salutation ", " name))
 
 (define-page (main-page-path)
-  (standard-page (get-greeting-string "Hello" ($ 'whom "world"))))
+  (standard-page (get-greeting-string "Hello" "world")))
